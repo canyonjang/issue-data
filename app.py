b1575627f23b78053f2f8a432653d814dada6c6a
canyonjang <114268359+canyonjang@users.noreply.github.com>
2026-03-27 13:35:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 # ==========================================
 # [교수님 설정 구역]
 # ==========================================
-CURRENT_ISSUE_NAME = "이슈2" 
+CURRENT_ISSUE_NAME = "이슈1" 
 PROFESSOR_PASSWORD = "3383"
 # ==========================================
 
